b89b3a0952bffe7dae64a0972eff9df65392276b
Matti Uusitalo <uusitalo.matti@gmail.com>
2020-05-30 14:34:11 +0300

    Funktiot viikon alun selvittämiseksi ja päivän kasvattamiseksi

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,12 +1,29 @@
 (ns app.main
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom])
+  (:import [goog.date Date Interval]))
 
-(def app (r/atom {}))
+(def app (r/atom {:startdate (Date.)}))
+
+(defn date->last-monday [date]
+  (let [day (.getDate date)
+        weekday (.getIsoWeekday date)
+        year (.getYear date)
+        month (.getMonth date)]
+    (Date. year month (- day weekday))))
+
+(defn inc-date [date days]
+  (let [new-date (.clone date)]
+    (->> (Interval. Interval/DAYS days)
+         (.add new-date))
+    new-date))
 
 (defn root []
   (fn []
-    [:h1 (:greeting @app)]) )
+    (let [monday (date->last-monday (:startdate @app))]
+      (into [:div]
+            (for [i (range 0 7)]
+              [:h1 (.toString (inc-date monday i))])))) )
 
 (defn ^:export main! []
   (rdom/render
