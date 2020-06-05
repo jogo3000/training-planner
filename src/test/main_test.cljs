2283d910812129d1c4cd6354b6dc8cac2e6f2a3b
Matti Uusitalo <uusitalo.matti@gmail.com>
2020-06-05 22:42:10 +0300

    Pari testiä uuden harkan lisäämiselle

--- src/test/main_test.cljs
+++ src/test/main_test.cljs
@@ -20,3 +20,33 @@
             actual (round-trip exercises)]
         (is (= (make-comparable exercises)
                (make-comparable actual)))))))
+
+
+(deftest exercise-editing
+  (testing "New exercise is added to an empty db"
+    (let [db (sut/handle :create-exercise {:editor "PK 10 km"
+                                           :exercises {}})
+          exercises (:exercises db)]
+      (is (= 1 (count exercises)))
+
+      (let [[id ex] (first exercises)]
+        (is (= id (:selected-element db)))
+        (is (= id (:id ex)))
+        (is (= {:x 0 :y 0 :z 0 :description "PK 10 km" :id id}
+               ex)))))
+
+  (testing "New exercise is added to a db with existing exercise"
+    (let [existing-id (random-uuid)
+          db (sut/handle :create-exercise {:editor "PK 10 km"
+                                           :exercises {existing-id {:id existing-id}}})
+          exercises (:exercises db)]
+      (is (= 2 (count exercises)))
+      (let [new-exercise-id (->> (keys exercises)
+                                 (filter #(not= existing-id %))
+                                 first)
+            exercise (get exercises new-exercise-id)]
+        (is (= new-exercise-id (:id exercise)))
+        (is (= {:id new-exercise-id
+                :x 0 :y 0 :z 0 :description "PK 10 km"}
+               exercise))
+        (is (= new-exercise-id (:selected-element db)))))))
