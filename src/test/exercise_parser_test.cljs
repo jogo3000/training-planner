a855e098352d91c53cf3c74aed89402885335378
Matti Uusitalo <uusitalo.matti@gmail.com>
2020-06-19 06:34:10 +0300

    Instaparsella yksinkertaiset aika- ja matkamääreet

--- src/test/exercise_parser_test.cljs
+++ src/test/exercise_parser_test.cljs
@@ -1,6 +1,6 @@
 (ns exercise-parser-test
   (:require [clojure.test :include-macros true :refer [deftest is testing]]
-            [exercise-parser :refer [parse-exercise]]))
+            [exercise-parser :refer [parse-exercise parser]]))
 
 
 (deftest exercise-text-parsing
@@ -12,3 +12,24 @@
 
     (testing "Parsing length measures with decimal units"
       (is (= 5880 (read-volume "5,88 km"))))))
+
+(deftest valid-exercises
+  (letfn [(valid? [s]
+            (= :S (first (parser s))))]
+    (is (valid? "10 km"))
+    (is (valid? "10000 m"))
+
+    (is (valid? "10'"))
+    (is (valid? "10min"))
+
+    (is (valid? "10h"))
+    (is (valid? "10:"))
+    (is (valid? "10 h"))
+
+    (is (valid? "15''"))
+    (is (valid? "15 s"))
+
+
+    (is (valid? "10'45"))
+    (is (valid? "10'10''"))
+    (is (valid? "10 h 10 min"))))
